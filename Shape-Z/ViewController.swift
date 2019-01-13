d4b148dfece6bdac34284686d9916dadb7801ccb
Markus Moenig <markus@moenig.io>
2019-01-13 08:58:10 +0700

    Initial Commit

---
+++ Shape-Z/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Shape-Z
+//
+//  Created by Markus Moenig on 13/1/19.
+//  Copyright © 2019 Markus Moenig. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
