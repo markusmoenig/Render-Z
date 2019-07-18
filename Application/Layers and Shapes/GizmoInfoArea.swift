feeb4814b251ff99958f9bdefca39dceebe970d8
Markus Moenig <markus@moenig.io>
2019-07-18 14:03:19 +0700

    GizmoInfoArea undo for object instances

--- Application/Layers and Shapes/GizmoInfoArea.swift
+++ Application/Layers and Shapes/GizmoInfoArea.swift
@@ -285,12 +285,33 @@ class GizmoInfoArea {
                         func applyProperties(_ object: Object,_ old: [String:Float],_ new: [String:Float])
                         {
                             self.gizmo.mmView.undoManager!.registerUndo(withTarget: self) { target in
-                                object.properties = old
                                 
+                                if object.instanceOf == nil {
+                                    object.properties = old
+                                } else {
+                                    if let layer = self.gizmo.app.nodeGraph.getLayerOfInstance(object.uuid) {
+                                        for inst in layer.objectInstances {
+                                            if inst.uuid == object.uuid {
+                                                inst.properties = old
+                                            }
+                                        }
+                                    }
+                                }
                                 applyProperties(object, new, old)
                             }
                             self.gizmo.app.updateObjectPreview(self.gizmo.rootObject!)
                             self.gizmo.mmView.update()
+                        }
+                        
+                        if object.instanceOf != nil {
+                            // This is an instance, we need to update the instance properties
+                            if let layer = self.gizmo.app.nodeGraph.getLayerOfInstance(object.uuid) {
+                                for inst in layer.objectInstances {
+                                    if inst.uuid == object.uuid {
+                                        inst.properties = object.properties
+                                    }
+                                }
+                            }
                         }
                         
                         applyProperties(object, self.gizmo.undoProperties, object.properties)
